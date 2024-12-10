54c48639fe469e0d37531767b16f0d2c483e0183
Thevutman <santiagovelascomosquera@hotmail.com>
2024-12-10 09:51:45 -0500

    [Fix Auth] - add auth.jsx 'removible' - add welcome.jsx 'se removera' - add index.js /constants 'Aqui estan las variables que conecatan la base de datos' - add /lib/supabase.js 'aqui esta la configuracion de la base de datos' Nota: Se añadio SupaBase, asi que se despues del pull deberian hacer un 'npm i'}

--- my-app/app/index.jsx
+++ my-app/app/index.jsx
@@ -1,10 +1,13 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
+import { useRouter } from 'expo-router'
 
 const index = () => {
+  const router = useRouter()
   return (
     <View>
-      <Text>index</Text>
+      <Text>indexx</Text>
+      <Button title="ir"  onPress={()=> router.push('Auth')}/>
     </View>
   )
 }
---
+++ my-app/app/welcome.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const welcome = () => {
+  return (
+    <View>
+      <Text>welcomee</Text>
+    </View>
+  )
+}
+
+export default welcome
