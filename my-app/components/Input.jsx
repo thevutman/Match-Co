801b7d048ada1ad2d6a4a1b149722bae2d5da983
Thevutman <santiagovelascomosquera@hotmail.com>
2024-12-17 10:35:59 -0500

    [Solucion Errores Fercho] - Error en 'BackButton.jsx' faltaba importar theme - Error en 'Input.jsx' faltaba importar theme - Error en 'login.jsx' Error al importar Button (no era el de componentes) - Error en 'signUp.jsx' Error al importar Button (no era el de componentes), Error de tipografia, se llamaba a 'loding' y era 'loading'

--- my-app/components/Input.jsx
+++ my-app/components/Input.jsx
@@ -1,36 +1,38 @@
-// import { StyleSheet, Text, TextInput, View } from 'react-native'
-// import React from 'react'
+import { StyleSheet, Text, TextInput, View } from 'react-native'
+import React from 'react'
+import { hp } from '@/helpers/common'
+import { theme } from '@/constants/theme'
 
-// const Input = (props) => {
-//   return (
-//     <View style={[styles.container, props.containerStyles && props.containerStyles]}>
-//       {
-//         props.icon && props.icon
-//       }  
-//       <TextInput
-//         style={{flex:1}}
-//         placeholderTextColor={theme.colors.textLight}
-//         ref={props.inputRef && props.inputRef}
-//         {...props}
-//         />
-//     </View>
-//   )
-// }
+const Input = (props) => {
+  return (
+    <View style={[styles.container, props.containerStyles && props.containerStyles]}>
+      {
+        props.icon && props.icon
+      }  
+      <TextInput
+        style={{flex:1}}
+        placeholderTextColor={theme.colors.textLight}
+        ref={props.inputRef && props.inputRef}
+        {...props}
+        />
+    </View>
+  )
+}
 
-// export default Input
+export default Input
 
-// const styles = StyleSheet.create({
-//     container:{
-//         flexDirection: 'row',
-//         height: hp(7.2),
-//         alignItems: 'center',
-//         justifyContent:'center',
-//         borderWidth: 0.4,
-//         borderColor: theme.colors.text,
-//         borderRadius: theme.radius.xxl,
-//         borderCurve: 'continous',
-//         paddingHorizontal: 18,
-//         gap:12
+const styles = StyleSheet.create({
+    container:{
+        flexDirection: 'row',
+        height: hp(7.2),
+        alignItems: 'center',
+        justifyContent:'center',
+        borderWidth: 0.4,
+        borderColor: theme.colors.text,
+        borderRadius: theme.radius.xxl,
+        borderCurve: 'continous',
+        paddingHorizontal: 18,
+        gap:12
     
-//     }
-// })
+    }
+})
