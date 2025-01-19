a68b082776211981db49a1d077ec3e75a3902265
Thevutman <santiagovelascomosquera@hotmail.com>
2025-01-19 16:16:09 -0500

    Prueba simon

--- my-app/app/signUp.jsx
+++ my-app/app/signUp.jsx
@@ -9,6 +9,7 @@ import { useRouter } from 'expo-router'
 import { hp, wp } from '@/helpers/common'
 import Input from '@/components/Input'
 import Button from '@/components/Button'
+import { supabase } from '../lib/supabase'
 
 
 const signUp = () => {
@@ -23,7 +24,25 @@ const signUp = () => {
         Alert.alert('Sign Up', "please fill all the fields!");
         return;
       }
-      //good  to go
+      
+      let name = nameRef.current.trim();
+      let email = emailRef.current.trim();
+      let password = passwordRef.current.trim();
+      
+      setLoading(true);
+      
+      const {data: {session}, error} = await supabase.auth.signUp({
+        email,
+        password,
+      })
+      
+      setLoading(false);
+
+      console.log('session: ', session)
+      console.log('error: ', error)
+      if(error){
+        Alert.alert('Sign Up', error.message);
+      }
     }
 
   return (
--- my-app/components/Loading.jsx
+++ my-app/components/Loading.jsx
@@ -1,15 +1,15 @@
-// import { StyleSheet, Text, View } from 'react-native'
-// import React from 'react'
-// import { theme } from '../constants/theme'
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { theme } from '../constants/theme'
 
-// const Loading = ({size="large",color=theme.colors.primary}) => {
-//   return (
-//     <View style = {{justifyContent: 'center', alignItems: 'center'}}>
-//       <ActivityIndacato size = {size} color={color}/>
-//     </View>
-//   )
-// }
+const Loading = ({size="large",color=theme.colors.primary}) => {
+  return (
+    <View style = {{justifyContent: 'center', alignItems: 'center'}}>
+      <ActivityIndicator size={size} color={color} />
+    </View>
+  )
+}
 
-// export default Loading
+export default Loading
 
-// const styles = StyleSheet.create({})
+const styles = StyleSheet.create({})
